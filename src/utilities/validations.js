e2805d9ba0638613f0df3aeab066318c630d613e
kompromaiz <kanin.alexey@gmail.com>
2021-03-15 01:45:50 +0300

    fixed validation bug

--- src/utilities/validations.js
+++ src/utilities/validations.js
@@ -1,5 +1,6 @@
 export const areAllFieldsValid = items => {
   for (let item of items) {
+    if(item.lastValue === undefined) return true;
     if(item.lastValue.includes("_")) return false;
   }
 
